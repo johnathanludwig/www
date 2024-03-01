496ed09c1b15b726843da8a3f1df7b5e896b933b
Johnathan Ludwig <john@johnathan.dev>
2024-03-01 18:52:23 -0500

    Add tailwindcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.css';
+</script>
+
+<header>
+	<nav>
+		<a href="/">Johnathan Ludwig</a>
+		<a href="/about">About</a>
+		<a href="/blog">Blog</a>
+		<a href="/experience">Experience</a>
+	</nav>
+</header>
+
+<slot />
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+import { sveltekit } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	preprocess: vitePreprocess()
 });
